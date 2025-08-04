260bfb0c364d895e2a86f5c043139afec42d9a1b
leni8ec <leni8ec@gmail.com>
2025-08-05 01:37:49 +0300

    feat(framework): add `ReactiveNumber` implementation

---
+++ Assets/Scripts/Framework/Reactive/ReactiveNumber.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Asteroids.Framework.Reactive {
+    /// <summary>
+    /// Integer Reactive property with an arithmetic operators overloading
+    /// </summary>
+    [Serializable]
+    public class ReactiveInt : ReactiveProperty<int> {
+
+        public static ReactiveInt operator ++(ReactiveInt self) {
+            self.Value++;
+            return self;
+        }
+
+        public static ReactiveInt operator --(ReactiveInt self) {
+            self.Value--;
+            return self;
+        }
+
+        public static ReactiveInt operator -(ReactiveInt self, int value) {
+            self.Value -= value;
+            return self;
+        }
+
+        public static ReactiveInt operator +(ReactiveInt self, int value) {
+            self.Value += value;
+            return self;
+        }
+
+        public static ReactiveInt operator *(ReactiveInt self, int value) {
+            self.Value *= value;
+            return self;
+        }
+
+        public static ReactiveInt operator /(ReactiveInt self, int value) {
+            self.Value /= value;
+            return self;
+        }
+
+    }
+
+    /// <summary>
+    /// Float Reactive property with an arithmetic operators overloading
+    /// </summary>
+    [Serializable]
+    public class ReactiveFloat : ReactiveProperty<float> {
+
+        public static ReactiveFloat operator ++(ReactiveFloat self) {
+            self.Value++;
+            return self;
+        }
+
+        public static ReactiveFloat operator --(ReactiveFloat self) {
+            self.Value--;
+            return self;
+        }
+
+        public static ReactiveFloat operator -(ReactiveFloat self, float value) {
+            self.Value -= value;
+            return self;
+        }
+
+        public static ReactiveFloat operator +(ReactiveFloat self, float value) {
+            self.Value += value;
+            return self;
+        }
+
+        public static ReactiveFloat operator *(ReactiveFloat self, float value) {
+            self.Value *= value;
+            return self;
+        }
+
+        public static ReactiveFloat operator /(ReactiveFloat self, float value) {
+            self.Value /= value;
+            return self;
+        }
+
+    }
+}

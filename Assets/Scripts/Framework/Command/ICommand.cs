c5d3cecbc15bc615215c8cf7cb60289cdebf6bd4
leni8ec <leni8ec@gmail.com>
2025-08-12 01:55:17 +0300

    docs: `ICommand` naming variants changes

--- Assets/Scripts/Framework/Command/ICommand.cs
+++ Assets/Scripts/Framework/Command/ICommand.cs
@@ -1,9 +1,10 @@
 ﻿namespace Asteroids.Framework.Command {
     // todo-naming:
     // - Command: it is not a command in its original form
-    // - Signal: more suitable
-    // - Message: too generic
+    // - Signal: the signal only notifies about something, but does not do the operation.
     // - Action: too generic, but it's close in meaning
+    // - Operation/Process: too veiled
+    // - Message: too generic
     // - Event: conflict with c# events, but it's close in meaning
     public interface ICommand { }
 }
